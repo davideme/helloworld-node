de5dcf9e13c139f6b366c227aaa507630384486e
Davide Mendolia <davideme@users.noreply.github.com>
2024-09-14 18:39:01 +0000

    listen to 0.0.0.0

--- server.mjs
+++ server.mjs
@@ -8,8 +8,8 @@ const server = createServer((req, res) => {
 
 // starts a simple http server locally on port from environment variable or 3000
 const port = process.env.PORT || 3000;
-server.listen(port, '127.0.0.1', () => {
-  console.log(`Listening on 127.0.0.1:${port}`);
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Listening on 0.0.0.0:${port}`);
 });
 
 // run with `node server.mjs`
